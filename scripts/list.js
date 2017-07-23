60f42fbaa051a1d6f682f9a33cd97376bfee2eb7
Clycheng <859651890@qq.com>
2017-07-23 22:21:09 +0800

    list

--- scripts/list.js
+++ scripts/list.js
@@ -15,20 +15,60 @@ $.ajax({
     }
 });
 //获取内容列表
-function shop_good() {
     var cat_id = GetQueryString("id");
     $.ajax({
-        "url": "http://h6.duchengjiu.top/shop/api_goods.php?pagesize=1&cat_id"+cat_id,
+        "url": "http://h6.duchengjiu.top/shop/api_goods.php?cat_id"+cat_id,
         "type": "get",
         "dataType": "JSON",
         "success": function(response) {
             console.log(response);
-            for (var i = 0; i = response.data.length; i ++){
-               var a =  $(".shop-list-content").clone();
-               console.log(a);
+            var length = response.data.length;
+            //获取节点 并且克隆对应的数量
+            var shopList = document.querySelector(".shop-list-content");
+            for (var i = 1; i < length; i ++){
+                 var cNode = shopList.cloneNode(true);
+                $("#shop-list-warp").append(cNode);
+            }
+            //获取新的列表内容 存倒数组
+            var newShopList = document.querySelectorAll(".shop-list-content");
+            var oImgArr = [];
+            var oAArr = [];
+            var oHArr = [];
+            var oPArr = [];
+            var priceArr = [];
+            for (var j = 0; j < newShopList.length; j ++){
+                var oA = newShopList[j].getElementsByTagName("a")[0];
+                var oImg = newShopList[j].getElementsByTagName("img")[0];
+                var oH = newShopList[j].getElementsByTagName("h3")[0];
+                var oP = newShopList[j].getElementsByTagName("p")[0];
+                var price = newShopList[j].getElementsByTagName("b")[0];
+                oAArr.push(oA);
+                oImgArr.push(oImg);
+                oHArr.push(oH);
+                oPArr.push(oP);
+                priceArr.push(price);
+            }
+            console.log(oAArr);
+            for (var k = 0; k < response.data.length; k ++) {
+                var obj = response.data[k];
+              oAArr[k].href = "detail.html"+obj.cat_id;
+              oImgArr[k].src = obj.goods_thumb;
+              oHArr[k].innerText = obj.goods_desc;
+              oPArr[k].innerText = obj.goods_name;
+              priceArr[k].innerText = "￥" + obj.price;
             }
         }
-    })
-}
-shop_good();
+    });
+//懒加载
+$(document).scroll(function(){
+    var $this =$(this),
+        viewH =$(this).height(),//可见高度
+        contentH =$(this).get(0).scrollHeight,//内容高度
+        scrollTop =$(this).scrollTop();//滚动高度
+    //if(contentH - viewH - scrollTop <= 100) { //到达底部100px时,加载新内容
+    if( scrollTop = viewH-contentH) {
+        console.log(1);
+    }
+});
+
 
